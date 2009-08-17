f7d0a96e11621fc47f6001ae437638e575367f04
Eric Smith <eric@8thlight.com>
2009-08-17 18:08:02 -0500

    Finished with walkthrough

--- walkthrough/props.rb
+++ walkthrough/props.rb
@@ -70,5 +70,53 @@ canvas do
         screenshot :image => "images/screenshot.jpg", :height => 300, :scaled => false, :players => "image"
       end
     end
+    
+    slide do
+      heading :text => "5. Hello Limelight"
+      directions :text => "You have the default scene running, but that's hardly fun.  Close that application and let's make a Hello Limelight application.  First you'll need to open the directory you created in step 4 in your favorite code editor.  Open the props.rb file in the default_scene.  You'll see this:"
+      codeblock do
+        code :text => "root :text => \"This is the Default Scene scene.\""
+      end
+      
+      directions :text => "We could change that to read 'Hello World!' but that's not useful.  Instead let's change the button to say Click Me and change its text when it's clicked.  First change the text to read:"
+      codeblock do
+        code :text => "root :text => \"Click Me!\""
+      end
+      
+      directions :text => "When you've changed the code reopen the application and make sure your change took effect, then proceed to the next step."
+    end
+    
+    slide do
+      heading :text => "6. A Player"
+      directions :text => "In order to add behaviors to props on the stage you need to create a player.  Fortunately that's simple.  The default_scene directory has another subdirectory underneath it named players.  In that directory create a file called root.rb, which will automatically add a player to any props named root.  Type the following code:"
+      codeblock do
+        code :text => "module Root"
+        code :text => "  def mouse_clicked(e)"
+        code :text => "    self.text = 'Hello World!'"
+        code :text => "  end"
+        code :text => "end"
+      end
+      
+      directions :text => "Save that file, then reopen your new limelight production and click the link.  Then move to the next step."
+    end
+    
+    slide do
+      heading :text => "7. A Pretty Player"
+      directions :text => "Okay you've made some interactivity - now let's make that a little more interesting.  Open the styles.rb file in the default_scene directory and edit the root style.  It can look like this:"
+      codeblock do
+        code :text => "rounded_corner_radius 10"
+        code :text => "padding 10"
+        code :text => "background_color \"#fffa\""
+        code :text => "secondary_background_color \"#fff6\""
+        code :text => "gradient :on"
+        code :text => "gradient_angle 270"
+      end
+      
+      directions :text => "After making those changes close and reopen your application.  You should have something that looks like this:"
+      screenshot_div do
+        screenshot :image => "images/screenshot2.jpg", :players => "image"
+      end
+      directions :text => "Congratulations!  You've made your first Limelight application.  Now why don't you start by tweaking that app to look pretty?"
+    end
   end
 end
