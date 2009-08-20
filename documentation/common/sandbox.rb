beb3e7b1238adffe80cf24da10b1e75242dd30bb
Jim Suchy <jsuchy@gmail.com>
2009-08-20 16:10:40 -0500

    extract the sandbox into a common component

---
+++ documentation/common/sandbox.rb
@@ -0,0 +1,6 @@
+canvas :id => "canvas", :width => "100%", :height => "200", :top_margin => "15", :bottom_margin => "15" do
+  prop :text => "Your props will appear here"
+end
+
+text_area :id => "code", :width => "95%", :margin => "2%"
+sandbox_button :players => "button", :text => "Illuminate", :id => "sandbox_button"
--- documentation/entrances/sandbox.rb
+++ documentation/entrances/sandbox.rb
@@ -20,9 +20,4 @@ canvas :id => "directions", :height => "290", :vertical_scrollbar => :on do
   end
 end
 
-canvas :id => "canvas", :width => "100%", :height => "200", :top_margin => "15", :bottom_margin => "15" do
-  prop :text => "Your props will appear here"
-end
-
-text_area :id => "code", :width => "95%", :margin => "2%"
-sandbox_button :players => "button", :text => "Illuminate", :id => "sandbox_button"
+__install "documentation/common/sandbox.rb"
