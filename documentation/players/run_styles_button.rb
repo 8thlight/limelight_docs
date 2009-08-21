c437859c11642294c0ec1a22b929a28a27db1cc2
Eric Smith <eric@8thlight.com>
2009-08-21 17:19:52 -0500

    We now can use a style thingy

--- documentation/players/run_styles_button.rb
+++ documentation/players/run_styles_button.rb
@@ -1,12 +1,24 @@
 module RunStylesButton
   def button_pressed(e)
+    styles = styles_from_text_area
+
+    apply_styles_to_canvas(styles)
+  end
+  
+  def styles_from_text_area
     code_area = scene.find("code")
-    
-    styles = Limelight.build_styles do
-      eval("prop {text_color :blue}")
+    return Limelight.build_styles do
+      eval(code_area.text)
     end
-    
-    test_prop = scene.find("test_prop")
-    test_prop.style = styles.values[0]
+  end
+  
+  def apply_styles_to_canvas(styles)
+    canvas = scene.find("canvas")
+    styles.each_pair do |prop_name, style|
+      canvas.find_by_name(prop_name).each do |prop|
+        prop.style.removeScreen()
+        prop.style.applyScreen(style)
+      end
+    end
   end
 end
--- spec/documentation/styles_spec.rb
+++ spec/documentation/styles_spec.rb
@@ -21,24 +21,84 @@ describe "Styles Tutorial" do
     slides.should_not be_empty
   end
   
-  it "should convert the text to a style - and apply it to the object" do
-    slideshow = scene.find('slideshow')
-    slideshow.next
-    slideshow.next
+  describe "Style Sandbox" do
+    before(:each) do
+      slideshow = scene.find('slideshow')
+      slideshow.next
+      slideshow.next
+      
+      @styles_sandbox_button = scene.find("run_styles_button")
+      @code_area = scene.find('code')
+    end
     
-    styles_sandbox_button = scene.find("run_styles_button")
-    code_area = scene.find('code')
+    it "should convert the text to a style - and apply it to the object" do
+      @code_area.text = "prop { text_color :blue }"
+      @styles_sandbox_button.button_pressed(nil)
 
-    style_dsl =<<END
-    prop {
-      text_color :blue
-    }
-END
+      prop = scene.find("test_prop")
+      prop.style.text_color.should == "#0000ffff"
+    end
     
-    code_area.text = style_dsl
-    styles_sandbox_button.button_pressed(nil)
+    it "should apply to the prop specified in the style" do
+      canvas = scene.find('canvas')
+      my_prop = Limelight::Prop.new(:name => "my_prop", :id => "my_prop")
+      my_prop.style.text_color = :blue
+      canvas << my_prop
+      
+      @code_area.text = "my_prop { text_color :red }"
+      @styles_sandbox_button.button_pressed(nil)
+      
+      prop = scene.find('my_prop')
+      prop.style.text_color.should == "#ff0000ff"
+    end
     
-    prop = scene.find("test_prop")
-    prop.style.text_color.should == "#0000ffff"
-  end
+    it "should apply to all the props with that name" do
+      canvas = scene.find('canvas')
+      my_prop = Limelight::Prop.new(:name => "my_prop", :id => "my_prop")
+      my_prop.style.text_color = :red
+      canvas << my_prop
+      my_prop2 = Limelight::Prop.new(:name => "my_prop", :id => "my_prop2")
+      my_prop2.style.text_color = :green
+      canvas << my_prop2
+      
+      @code_area.text = "my_prop { text_color :blue }"
+      @styles_sandbox_button.button_pressed(nil)
+
+      props = scene.find_by_name("my_prop")
+      props[0].style.text_color.should == "#0000ffff"
+      props[1].style.text_color.should == "#0000ffff"
+    end
+    
+    it "should handle when the prop in the style isnt in the scene" do
+      style_dsl = "my_prop { text_color :blue }"
+
+      @code_area.text = style_dsl
+      lambda{@styles_sandbox_button.button_pressed(nil)}.should_not raise_error
+    end
+    
+    it "should only apply the styles to the children of the canvas" do
+      my_prop = Limelight::Prop.new(:name => "my_prop", :id => "my_prop")
+      my_prop.style.text_color = :red
+      scene << my_prop
+      
+      @code_area.text = "my_prop { text_color :blue }"
+      
+      @styles_sandbox_button.button_pressed(nil)
+      my_prop.style.text_color.should == "#ff0000ff"
+    end
+    
+    it "should work properly if run twice - removing the screen then adding it" do
+      canvas = scene.find('canvas')
+      my_prop = Limelight::Prop.new(:name => "my_prop", :id => "my_prop")
+      my_prop.style.text_color = :red
+      canvas << my_prop
+      
+      @code_area.text = "my_prop { text_color :blue }"
+      @styles_sandbox_button.button_pressed(nil)
+      @code_area.text = "my_prop { text_color :red }"
+      @styles_sandbox_button.button_pressed(nil)
+      
+      my_prop.style.text_color.should == "#ff0000ff"
+    end
+  end  
 end
