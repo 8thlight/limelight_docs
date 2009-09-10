8daa79d851406fc6d2f8d2efa4312fc33dfe8bc2
Eric Smith <eric@8thlight.com>
2009-09-10 12:31:12 -0500

    Some changes for the players tutorial

--- documentation/entrances/slideshow/players.rb
+++ documentation/entrances/slideshow/players.rb
@@ -1,11 +1,11 @@
 slide do
   line_break
-  directions :text => "All the worlds a stage and .... you get the drill.  Right now your props may look pretty, but they can't do anything.  To add behavior to a prop you create a Player, which is just a Ruby module with the same name as your prop.  Let's dive into that right now."
+  directions :text => "All the worlds a stage and .... you get the drill.  Right now your props may look pretty, but they can't do anything.  To add behavior to a Prop you create a Player, which is just a Ruby module that contains the Prop's behavior.  Let's dive into that right now."
 end
 
 slide do
   heading :text => "Who are the Players?"
-  directions :text => "A Player is just a Ruby module that is linked to it's Prop through it's name.  For example this very application has a Player named NextButton that looks like this:"
+  directions :text => "To link a Player to a Prop, you create a module with the same name as that Prop.  For example this very application has a Player named NextButton that looks like this:"
   
   codeblock do
     code :text => "module NextButton"
@@ -16,19 +16,19 @@ slide do
   end
   
   line_break
-  directions :text => "Each time Prop named next_button is placed in the scene it will aquire this behavior.  In the case advancing the slideshow."
+  directions :text => "Now any Prop named next_button has this behavior, in the case advancing the slideshow."
 end
 
 slide do
   heading :text => "Player File"
-  directions :text => "Each Limelight Scene has a directory named Players, which contains a Ruby module for each Player.  When that Scene is loaded it will attach the the Players to the Props in the Scene based on Prop names.  In addition there can be Production level Players.  Those are created by creating a players directory in the root directory of the Production, and putting players in it."
-  
+  directions :text => "Each Scene has a directory named Players, which contains a Ruby module for each Player, and when the Scene is loaded it will attach the the Players to the Props in the Scene.  In addition there can be Production level Players.  Those are created by creating a players directory in the root directory of the Production, and putting Players in it."
+  line_break
   screenshot :image => "images/players.jpg", :players => "image"
 end
 
 slide do
   heading :text => "Testing"
-  directions :text => "Testing Player behavior is done using the RSpec tests generated with the limelight production.  In order to test a Player the simplest thing to do is load the Scene and find the Prop, at which point you call methods on the Prop directly.  You saw this test already:"
+  directions :text => "Testing Player behavior is done using the RSpec tests generated with the Production.  In order to test a Player the simplest thing to do is load the Scene and find the Prop, at which point you call methods on the Prop directly.  You saw this test already:"
   
   codeblock do
     code :text => "it 'should change the root scene text when it is clicked' do"
@@ -39,11 +39,44 @@ slide do
   end
   
   line_break
-  directions :text => "The uses scene method to get the scene then finds the "
+  directions :text => "Every spec has a scene method assuming you use the proper directive at the top of your describe block:" 
+  
+  codeblock do
+    code :text => "it 'should change the root scene text when it is clicked' do"
+    code :text => "  root = scene.children[0]"
+    code :text => "  root.mouse_clicked(nil)"
+    code :text => "  root.text.should == \"Hello Limelight!\""
+    code :text => "end"
+  end
+  
+  line_break
+  directions :text => "Finally each spec for a Scene has the following directive just below the describe block:"
+  
+  codeblock do
+    code :text => "describe \"MyScene\""
+    code :text => "  uses_scene :my_scene"
+  end
+  
+  line_break
+  directions :text => "This will load the Scene for you, and you can use the scene method to access it in your specs."
 end
 
 slide do
-  heading :text => "Hooks..."
+  heading :text => "Events"
+  mouse_clicked - Invoked when the uses clicks on a Prop. More specifically, it's invoked when a mouse button is pressed and released all the while the location of the mouse is in the bounds of the Prop.
+  mouse_entered - Invoked when the mouse enters the bounds of a Prop.
+  mouse_exited - Invoked when the mouse, previously inside the Prop, move outside the bounds of the Prop.
+  mouse_pressed - Invoked when a mouse button is pressed down while the mouse's location is within the bounds of the Prop.
+  mouse_released - Invoked when a mouse button is released down while the mouse's location is within the bounds of the Prop.
+  mouse_dragged - Invoked when the mouse's location changes within the bounds of the Prop while a mouse button is depressed.
+  mouse_moved - Invoked when the mouse's location changes within the bounds of the Prop.
+  key_typed - Invoked when a key is typed. only applicable to built-in input players
+  key_pressed - Invoked when a key is pressed. only applicable to built-in input players
+  key_released - Invoked when a key is released. only applicable to built-in input players
+  focus_gained - Invoked when the keyboard focus is given to the Prop. only applicable to built-in input players
+  focus_lost - Invoked when the keyboard focus is taken from the Prop. only applicable to built-in input players
+  button_pressed - Invoked when a Prop's button is pressed. only applicable to the following built-in players: button, check_box, radio_button
+  value_changed - Invoked when the value of a combo_box Prop is changed. only applicable to the combo-box built-in player
 end
 
 slide do
@@ -56,6 +89,10 @@ end
 
 slide do
   heading :text => "Builtin Players"
+end
+
+slide do
+  heading :text => "Scene Manipulation"
 end
 
 slide do
