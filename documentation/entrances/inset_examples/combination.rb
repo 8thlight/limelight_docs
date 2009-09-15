24a77364fea7e37c8875043184ead053b596064c
Eric Smith <eric@8thlight.com>
2009-09-15 13:34:44 -0500

    Working combo example

--- documentation/entrances/inset_examples/combination.rb
+++ documentation/entrances/inset_examples/combination.rb
@@ -1,4 +1,8 @@
-left_prop :text => "I am the left text."
-right_prop do
-  inner_prop :text => "I am the right text."
+left_prop do
+  red_square :background_color => :red, :height => 30, :width => 30
+end
+right_prop :border_width => 1, :border_color => :black, :width => "60%", :height => "100%" do
+  inner_prop do
+    blue_square :background_color => :blue, :height => 30, :width => 30
+  end
 end
