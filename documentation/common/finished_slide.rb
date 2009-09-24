2d4f8dca291ee759c66af832b0374d831e627bba
Eric Meyer <ericmeyer@eric-meyers-macbook-pro.local>
2009-09-24 15:43:01 -0500

    work on final slide

--- documentation/common/finished_slide.rb
+++ documentation/common/finished_slide.rb
@@ -2,8 +2,10 @@ slide do
   next_walkthrough = WalkthroughLink.next(@slideshow)
   heading :text => "Finished"
   if next_walkthrough
-    directions :text => "Congratulations!  You completed this tutorial.  Feel free to play and experiment with it and when you're ready, head to the"
-    tutorial_link :id => "next_slideshow", :slideshow => next_walkthrough.slideshow, :title => next_walkthrough.title, :text => "next tutorial.", :styles => "directions_tutorial_link"
+    directions :text => "Congratulations!  You completed this tutorial.  Feel free to play and experiment with it and when you're ready, head to the next tutorial"
+    line_break
+    directions :text => "Next up is:"
+    tutorial_link :id => "next_slideshow", :slideshow => next_walkthrough.slideshow, :title => next_walkthrough.title, :text => " #{next_walkthrough.title}", :styles => "directions_tutorial_link"
   else
     directions :text => "Congratulations!  You completed the last tutorial.  Feel free to play and experiment with it and when you're ready, start your own Limelight Production."
   end
