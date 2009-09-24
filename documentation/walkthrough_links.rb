524687863538753dd0b040325b89a465277e9279
Eric Meyer <ericmeyer@eric-meyers-macbook-pro.local>
2009-09-24 10:36:26 -0500

    refactored walkthrough links

--- documentation/walkthrough_links.rb
+++ documentation/walkthrough_links.rb
@@ -1,19 +1,4 @@
-toc_link :id => "getting_started_walkthrough",  :text => "Getting Started", :slideshow => "getting_started", :title => "Getting Started"
-toc_link :id => "creating_props_walkthrough",   :text => "Creating Props",  :slideshow => "creating_props", :title => "Creating Props"
-toc_link :id => "text_walkthrough",             :text => "Text",            :slideshow => "text", :title => "Text"
-toc_link :id => "prop_partials_walkthrough",    :text => "Prop Partials",   :slideshow => "prop_partials", :title => "Prop Partials"
-toc_link :id => "editing_styles_walkthrough",   :text => "Editing Styles",  :slideshow => "editing_styles", :title => "Editing Styles"
-toc_link :id => "prop_layout_walkthrough",       :text => "Prop Layout",     :slideshow => "prop_layout", :title => "Prop Layout"
-toc_link :id => "float_walkthrough",             :text => "Floating Props",  :slideshow => "float", :title => "Floating Props"
-toc_link :id => "prop_sizing_walkthrough",       :text => "Prop Sizing",     :slideshow => "prop_sizing", :title => "Prop Sizing"
-toc_link :id => "colors_walkthrough",           :text => "Colors",          :slideshow => "colors", :title => "Colors"
-toc_link :id => "backgrounds_walkthrough",      :text => "Backgrounds",     :slideshow => "backgrounds", :title => "Backgrounds"
-toc_link :id => "gradients_walkthrough",        :text => "Gradients",       :slideshow => "gradients", :title => "Gradients"
-toc_link :id => "borders_walkthrough",          :text => "Borders",         :slideshow => "borders", :title => "Borders"
-toc_link :id => "insets_walkthrough",           :text => "Margin and Padding",  :slideshow => "insets", :title => "Insets"
-toc_link :id => "players_walkthrough",          :text => "Players",         :slideshow => "players", :title => "Players"
-toc_link :id => "images_walkthrough",           :text => "Images",          :slideshow => "images", :title => "Images"
-toc_link :id => "animations_walkthrough",       :text => "Animations",      :slideshow => "animations", :title => "Animations"
-toc_link :id => "sound_walkthrough",            :text => "Sound",           :slideshow => "sound", :title => "Sound"
-toc_link :id => "productions_walkthrough",      :text => "Productions",     :slideshow => "productions", :title => "Productions"
-toc_link :id => "stages_walkthrough",           :text => "Stages",          :slideshow => "stages", :title => "Stages"
+require "walkthrough_link"
+WalkthroughLink.all.each do |link|
+  toc_link :id => link.id,  :text => link.text, :slideshow => link.slideshow, :title => link.title
+end
