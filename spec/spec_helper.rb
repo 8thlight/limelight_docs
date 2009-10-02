43e6e37d5d57c7f580b257fe502458f200c1bcf4
Eric Smith <eric@8thlight.com>
2009-10-02 10:55:35 -0500

    pulled custom extension matcher into its own file

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,40 +12,7 @@ Dir.glob(File.join("__resources", "gems", "gems", "**", "lib")).each do |dir|
 end
 
 
-
-module HaveStyleExtensionMatcher
-  class HaveStyleExtension
-    def initialize(expected)
-      @expected = expected
-    end
-
-    def matches?(target)
-      @target = target
-      @target.style.has_extension(@target.scene.styles[@expected])
-    end
-
-    def failure_message
-      "expected <#{to_string(@target)}> to " +
-      "have the style extension '#{to_string(@expected)}'"
-    end
-
-    def negative_failure_message
-      "expected <#{to_string(@target)}> to " +
-      "not have the style extension '#{to_string(@expected)}'"
-    end
-
-    def to_string(value)
-      return 'nil' if value.nil?
-      return value.to_s
-    end
-  end
-
-  # Actual matcher that is exposed.
-  def have_style_extension(expected)
-    HaveStyleExtension.new(expected)
-  end
-end
-
+require "spec/custom_matchers/have_style_extension"
 
 Spec::Runner.configure do |config|
   config.include(HaveStyleExtensionMatcher)
