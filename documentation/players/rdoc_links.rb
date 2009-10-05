18412b0cac2c6b0d53419a0cab1d4cb6beb30e9c
Eric Smith <eric@8thlight.com>
2009-10-05 15:35:13 -0500

    Revert "Moved to using pop to adjust the list"
    This reverts commit be997f67abf86943c5c7996a1c115bbf761c61c2.

--- documentation/players/rdoc_links.rb
+++ documentation/players/rdoc_links.rb
@@ -2,38 +2,32 @@ module RdocLinks
   def casted
     @namespaces = []
     if scene.rdoc != nil
-      scene.rdoc.keys.sort.each { |class_name| @class_name = class_name; build_class_link }
+      scene.rdoc.keys.sort.each { |class_name| build_class_link_for(class_name) }
     end 
   end
   
   protected ###############################################
   
-  def build_class_link
-    write_section_headers
+  def build_class_link_for(class_name)
+    write_section_headers_for(class_name)
   end
   
-  def write_section_headers
+  def write_section_headers_for(class_name)
     if !@namespaces.empty?
       namespace_string = @namespaces.join("::").concat("::")
-      adjusted_class_name = @class_name.gsub(namespace_string, "")
+      adjusted_class_name = class_name.gsub(namespace_string, "")
     else
-      adjusted_class_name = @class_name
+      adjusted_class_name = class_name
     end
     
-    @split_class_name = adjusted_class_name.split('::')
-    @class_name = @split_class_name.pop
-    
-    @split_class_name.each do |name|
-      # if index != @split_class_name.size - 1
+    split_class_name = adjusted_class_name.split('::')
+    split_class_name.each_with_index do |name, index|
+      if index != split_class_name.size - 1
         self.build(:text => name) { class_header :text => @text }
         @namespaces << name
-      # end
+      end
     end
     
-    self.build(:class_text => @class_name) { class_link :text => @class_text }
+    self.build(:class_text => split_class_name.last) { class_link :text => @class_text }
   end
-  
-  # def split
-  #   
-  # end
 end
