33da407fcde92f5e1618d544349396bb1b577e89
Eric Smith <eric@8thlight.com>
2009-10-05 15:34:23 -0500

    Revert "I really only care about height"
    This reverts commit d2195fbcf5e54265ff29eb224d2615cc9e2193c2.

--- documentation/players/shrinkable.rb
+++ documentation/players/shrinkable.rb
@@ -1,17 +1,20 @@
 module Shrinkable
   def casted
     @height = self.style.height
+    @width = self.style.width
   end
   
   def shrink
     self.style.height = '0'
+    self.style.width = '0'
   end
   
   def grow
     self.style.height = @height
+    self.style.width = @width
   end
   
   def shrunk?
-    return true if self.style.height == '0'
+    return true if self.style.height == '0' and self.style.width == '0'
   end
 end
--- spec/documentation/players/shrinkable_spec.rb
+++ spec/documentation/players/shrinkable_spec.rb
@@ -12,6 +12,7 @@ describe "Shrinkable" do
     player.shrink
     
     player.style.height.should == '0'
+    player.style.width.should == '0'
   end
   
   it "should grow the height and width back to where they started" do
@@ -19,10 +20,12 @@ describe "Shrinkable" do
     player.grow
     
     player.style.height.should == 'auto'
+    player.style.width.should == 'auto'
   end
     
   it "should return shrunk as true when both height & width are 0" do
     player.style.height = '0'
+    player.style.width = '0'
     player.should be_shrunk
   end
 end
