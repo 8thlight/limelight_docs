0dbf5b44298676f04212aae2e033b58d05449046
Eric Smith <eric@8thlight.com>
2009-10-05 15:34:43 -0500

    Revert "Better.  Not good - still too many temporary variables, and doesnt read like the algorithm, but better"
    This reverts commit 0830ff3cbb7f1ebc49a5069420980804fad627fa.

--- documentation/players/rdoc_links.rb
+++ documentation/players/rdoc_links.rb
@@ -9,36 +9,31 @@ module RdocLinks
   protected ###############################################
   
   def build_class_link
-    write_section
-  end
-  
-  def write_section
-    @head, @tail = split_class
-    write_section_headers    
-    self.build(:class_text => @tail) { class_link :text => @class_text }
+    write_section_headers
   end
   
   def write_section_headers
-    @head.each do |name|
-      self.build(:text => name) { class_header :text => @text }
-      @namespaces.push name
-    end
-  end
-  
-  def split_class
-    split_class_name = adjusted_class_name
-    class_name = split_class_name.pop
-    return split_class_name, class_name
-  end
-  
-  def adjusted_class_name
     if !@namespaces.empty?
       namespace_string = @namespaces.join("::").concat("::")
-      name = @class_name.gsub(namespace_string, "")
+      adjusted_class_name = @class_name.gsub(namespace_string, "")
     else
-      name = @class_name
+      adjusted_class_name = @class_name
     end
     
-    return name.split('::')    
+    @split_class_name = adjusted_class_name.split('::')
+    @class_name = @split_class_name.pop
+    
+    @split_class_name.each do |name|
+      # if index != @split_class_name.size - 1
+        self.build(:text => name) { class_header :text => @text }
+        @namespaces << name
+      # end
+    end
+    
+    self.build(:class_text => @class_name) { class_link :text => @class_text }
   end
+  
+  # def split
+  #   
+  # end
 end
