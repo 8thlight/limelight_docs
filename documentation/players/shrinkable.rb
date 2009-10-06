cff8890ee91e3c6560c364242558149d063f10d8
Eric Smith <eric@8thlight.com>
2009-10-06 09:46:45 -0500

    start shrunk

--- documentation/players/shrinkable.rb
+++ documentation/players/shrinkable.rb
@@ -1,6 +1,9 @@
 module Shrinkable
+  attr_accessor :start_shrunk
+  
   def casted
     @height = self.style.height
+    self.shrink
   end
   
   def shrink
--- spec/documentation/players/shrinkable_spec.rb
+++ spec/documentation/players/shrinkable_spec.rb
@@ -8,21 +8,25 @@ describe "Shrinkable" do
     player.casted
   end
   
-  it "should shrink the height and width to zero on shrink" do
-    player.shrink
-    
-    player.style.height.should == '0'
-  end
-  
   it "should grow the height and width back to where they started" do
-    player.shrink
     player.grow
     
     player.style.height.should == 'auto'
   end
+  
+  it "should shrink the height and width to zero on shrink" do
+    player.grow
+    player.shrink
     
+    player.style.height.should == '0'
+  end
+      
   it "should return shrunk as true when both height & width are 0" do
     player.style.height = '0'
+    player.should be_shrunk
+  end
+  
+  it "should start things shrunk" do
     player.should be_shrunk
   end
 end
