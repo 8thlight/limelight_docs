6e45e40b3adcde9959b2b837434364ac73c611a8
Eric Smith <eric@8thlight.com>
2009-10-06 06:33:33 -0500

    Revert "Revert "Writing section headers, shrinking a section - kinda""
    This reverts commit 03d405e04c03c528ae472fafe62e27d22077a873.

--- documentation/styles.rb
+++ documentation/styles.rb
@@ -74,10 +74,19 @@ disabled_toc_heading {
   font_style :italic
 }
 
+section_header {
+  extends :toc_item
+  font_size 14
+  left_padding 8
+  top_padding 5
+  bottom_padding 4
+  hover {}
+}
+
 toc_tutorial_link {
   extends :toc_item
   font_size 12
-  left_padding 16
+  left_padding 22
   top_padding 2
   bottom_padding 2
   hover {
