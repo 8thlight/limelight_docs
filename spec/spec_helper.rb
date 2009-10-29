7bdfdd6b78f8b2ad13920bdb71d4e8333aea995b
Eric Meyer <emeyer@8thlight.com>
2009-10-29 15:24:43 -0500

    moved files

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,11 @@
-$: << File.expand_path(File.dirname(__FILE__) + "/../lib") # See if you can move this to lib spec helper
-$: << File.expand_path(File.dirname(__FILE__) + "/..")
+$: << File.expand_path(File.dirname(__FILE__) + "/../production/lib")
+$: << File.expand_path(File.dirname(__FILE__) + "/../production")
 
 require 'rubygems'
 require 'spec'
 require 'limelight/specs/spec_helper'
 
-$PRODUCTION_PATH = File.expand_path(File.dirname(__FILE__) + "/../")
+$PRODUCTION_PATH = File.expand_path(File.dirname(__FILE__) + "/../production")
 Gem.use_paths(File.join($PRODUCTION_PATH , "__resources", "gems"), Gem.default_path)
 
 Dir.glob(File.join("__resources", "gems", "gems", "**", "lib")).each do |dir|
