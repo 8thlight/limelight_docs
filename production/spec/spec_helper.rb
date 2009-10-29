56254fc9f85a7843b3c9a3b22a6bf9ed38d293b3
Eric Meyer <emeyer@8thlight.com>
2009-10-29 15:17:01 -0500

    fixed spec rake task

--- production/spec/spec_helper.rb
+++ production/spec/spec_helper.rb
@@ -1,10 +1,9 @@
 $: << File.expand_path(File.dirname(__FILE__) + "/../lib") # See if you can move this to lib spec helper
 $: << File.expand_path(File.dirname(__FILE__) + "/..")
 
-# $: << "/Users/eric/Projects/limelight/lib/"
-require 'limelight/specs/spec_helper'
 require 'rubygems'
 require 'spec'
+require 'limelight/specs/spec_helper'
 
 $PRODUCTION_PATH = File.expand_path(File.dirname(__FILE__) + "/../")
 Gem.use_paths(File.join($PRODUCTION_PATH , "__resources", "gems"), Gem.default_path)
